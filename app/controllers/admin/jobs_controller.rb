f6cfa1d144e21e4fe6c7480aa513332417fa04cc
louis <zyzlouis@gmail.com>
2017-04-13 21:10:40 +0800

    finishes step4 &mark in step5

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -7,7 +7,7 @@ class Admin::JobsController < ApplicationController
   end
 
   def index
-    @jobs = Job.all
+    @jobs = Job.all.recent
   end
 
   def new
@@ -50,6 +50,6 @@ class Admin::JobsController < ApplicationController
 
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email)
+    params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email, :is_hidden)
   end
 end
--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,10 @@
 module JobsHelper
+
+  def job_status(job)
+    if job.is_hidden
+      "前台隐藏"
+    else
+      "前台显示"
+    end
+  end
 end
