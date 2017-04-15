57633c80cf1bb3340239804d744a5ec0a610fffe
louis <zyzlouis@gmail.com>
2017-04-15 22:57:29 +0800

    implement resumes detail show and download function

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
+      resources :resumes
     end
   end
+
   resources :jobs do
     resources :resumes
   end
